421c7c20cfacc33a2382bc5cf5321a44747626d2
Mohammedekh <muhamadekhhaji99@gmail.com>
2023-03-03 00:02:43 +0300

    adding templates

---
+++ hirbate/routes.py
@@ -0,0 +1,12 @@
+from flask import render_template, url_for
+from hirbate import app
+
+
+
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+
+    return render_template('home.html', title='Home Page')
