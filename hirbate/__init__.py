e3b40dc146d1bb27898169bfdd7cc399f6f532a0
Mohammedekh <muhamadekhhaji99@gmail.com>
2023-03-04 11:32:02 +0300

    user registration page

--- hirbate/__init__.py
+++ hirbate/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ca756c5133bfd45cb5f1ff0c7a21d624'
 
 from hirbate import routes
--- hirbate/routes.py
+++ hirbate/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, flash
 from hirbate import app
+from hirbate.forms import RegistrationForm
 
 
 
@@ -10,3 +11,10 @@ from hirbate import app
 def home():
 
     return render_template('home.html', title='Home Page')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Submitted suscessfully' 'success')
+    return render_template('register.html', title='Sign Up', form=form)
