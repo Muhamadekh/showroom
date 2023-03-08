cf683aab48a60dbece9f02cf32efd131456b8380
Mohammedekh <muhamadekhhaji99@gmail.com>
2023-03-08 20:23:32 +0300

    User account and profile pictures

--- hirbate/forms.py
+++ hirbate/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from hirbate.models import User
+from flask_login import current_user
 
 
 class RegistrationForm(FlaskForm):
@@ -28,3 +30,25 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), Length(min=4)])
     submit = SubmitField('Login')
     remember = BooleanField('Remember Me')
+
+
+
+
+class UpdateAccountForm(FlaskForm):
+
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Upload Profile Photo', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Upload Photo')
+
+    def validate_username(self, username):
+        if current_user.username != username.data:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('This username is taken. Please choose another one.')
+
+    def validate_email(self, email):
+        if current_user.email != email.data:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('This email is taken. Please choose another one.')
--- hirbate/models.py
+++ hirbate/models.py
@@ -14,6 +14,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     date_added = db.Column(db.DateTime, default=datetime.utcnow())
+    image_file = db.Column(db.String, nullable=False, default='default.jpg')
     reference = db.relationship('Car', backref='Owner', lazy=True)
 
 
